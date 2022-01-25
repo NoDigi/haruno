70277ba43ec4920c01d4eb01128a26cfb5245c7c
NoDigi <98347766+NoDigi@users.noreply.github.com>
2022-01-25 11:24:35 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ GoPay: 628112958665
-├ OVO: 628112958665
-├ Dana: 628112958665
-├ Pulsa (XL): 6281943265086
+┌〔 Donasi • Topup 〕
+├ Digi✨: 60149431385
+├ Umobile✨: 60189830350
+├ Digi✨: 60149431385
+├ Umobile✨: 60189830350
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
